fd0bf63e71df9dd08bd90517003909d182632e21
EmmanuelAKpan013 <sundayemma013@gmail.com>
2022-08-15 14:55:45 +0100

    Landing Page Adjustment

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,21 @@
 import "./HomePage.css";
 import Navbar from "../components/Navbar";
+import designerImage from "../images/designerImage.png";
+import productDesignerText from "../images/product designer.png";
+
 function HomePage() {
   return (
     <>
       <Navbar />
+      <div className="designer-container">
+        <div className="designer-image">
+          <img src={designerImage} alt="" />
+        </div>
+        <div className="designer-text">
+          <img src={productDesignerText} alt="" />
+        </div>
+        <div className="designer-name">ONALO DEBORAH</div>
+      </div>
     </>
   );
 }
