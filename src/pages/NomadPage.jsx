d0b30b300cc931919f67829aabbc796c8f16b49a
EmmanuelAKpan013 <sundayemma013@gmail.com>
2022-09-14 13:28:13 +0100

    update

--- src/pages/NomadPage.jsx
+++ src/pages/NomadPage.jsx
@@ -127,44 +127,43 @@ function NomadPage() {
         </div>
       </div>
 
-      {/* <div className="work-overview">
-        <div className="work-over-text">
-          <h4>02</h4>
-          <br />
-          <h2>
-            Work <br />
-            OVerview
-          </h2>
-        </div>
-        <div className="work-over-text-para">
-          <div className="fastr">
-            <p>
-              <strong>Fastr</strong> is an enhanced digital financial service
-              that streamline and standardise the invoicing process and aids in
-              accurate record keeping making it easy to track income and
-              possibly expenses, saving time, work, error and omissions,
-              alleviating frustrations in record keeping; Analytics tools record
-              business performance, quantifying the value of early payments in
-              money and time. Early payment on invoices means SMEs can access
-              money owed within 48 hours rather than waiting 30, 60 or even 180
-              days!{" "}
-            </p>
+      <div className="work-overview">
+        <div>
+          <div className="work-over-text">
+            <h4>02</h4>
+            <br />
+            <h2>Work OVerview</h2>
           </div>
-          <br />
-          <div className="solution">
-            <h4>Solution</h4>
-            <p>
-              The goal is for Nomad Bank to offer a digital solution that is SME
-              focused Digital Banking and Unlocks Working Capital for these
-              MSMEs. This involved me working on an invoicing tool, purchase
-              order tool, a pay early feature and a credit building system as
-              these aid businesses in tracking transactions, solves late payment
-              issues and build enough credit to apply for loans - unlocking
-              working capital.
-            </p>
+          <div className="work-over-text-para">
+            <div className="fastr">
+              <p>
+                <strong>Fastr</strong> is an enhanced digital financial service
+                that streamline and standardise the invoicing process and aids
+                in accurate record keeping making it easy to track income and
+                possibly expenses, saving time, work, error and omissions,
+                alleviating frustrations in record keeping; Analytics tools
+                record business performance, quantifying the value of early
+                payments in money and time. Early payment on invoices means SMEs
+                can access money owed within 48 hours rather than waiting 30, 60
+                or even 180 days!{" "}
+              </p>
+            </div>
+            <br />
+            <div className="solution">
+              <h4>Solution</h4>
+              <p>
+                The goal is for Nomad Bank to offer a digital solution that is
+                SME focused Digital Banking and Unlocks Working Capital for
+                these MSMEs. This involved me working on an invoicing tool,
+                purchase order tool, a pay early feature and a credit building
+                system as these aid businesses in tracking transactions, solves
+                late payment issues and build enough credit to apply for loans -
+                unlocking working capital.
+              </p>
+            </div>
           </div>
         </div>
-      </div> */}
+      </div>
       {/* <div className="research">
         <div className="research-header-and-image">
           <div className="research-header">
