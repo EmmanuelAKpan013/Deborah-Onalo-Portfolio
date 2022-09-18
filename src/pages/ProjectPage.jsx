7ef47e7cf5c581e1cdeeb1f3b832ab37155c7ed7
EmmanuelAKpan013 <sundayemma013@gmail.com>
2022-09-18 21:49:36 +0100

    update

--- src/pages/ProjectPage.jsx
+++ src/pages/ProjectPage.jsx
@@ -8,49 +8,47 @@ function ProjectPage() {
   return (
     <>
       <Navbar />
-      <div className="projectpage-container">
-        <div className="project-header">selected projects</div>
-        <div className="project-lists">
-          <div
-            className="project-list item1"
-            onClick={() => {
-              navigate(`/project/nomadbank`);
-            }}
-          >
-            <p>Fastr - NomadBank</p>
-          </div>
-          <div
-            className="project-list item2"
-            onClick={() => {
-              navigate(`/project/blomblom`);
-            }}
-          >
-            <p>Website Redesign - Bloobloom</p>
-          </div>
-          <div
-            className="project-list item3"
-            onClick={() => {
-              navigate(`/project/saudipix`);
-            }}
-          >
-            <p>Saudipix</p>
-          </div>
-          <div
-            className="project-list item4"
-            onClick={() => {
-              navigate(`/project/investnow`);
-            }}
-          >
-            <p>InvestNow.ng Redesign</p>
-          </div>
-          <div
-            className="project-list item5"
-            onClick={() => {
-              navigate(`/project/watertalk`);
-            }}
-          >
-            <p className="listName">WaterTalk</p>
-          </div>
+      <div className="project-header">selected projects</div>
+      <div className="project-lists">
+        <div
+          className="project-list item1"
+          onClick={() => {
+            navigate(`/project/nomadbank`);
+          }}
+        >
+          <p>Fastr - NomadBank</p>
+        </div>
+        <div
+          className="project-list item2"
+          onClick={() => {
+            navigate(`/project/blomblom`);
+          }}
+        >
+          <p>Website Redesign - Bloobloom</p>
+        </div>
+        <div
+          className="project-list item3"
+          onClick={() => {
+            navigate(`/project/saudipix`);
+          }}
+        >
+          <p>Saudipix</p>
+        </div>
+        <div
+          className="project-list item4"
+          onClick={() => {
+            navigate(`/project/investnow`);
+          }}
+        >
+          <p>InvestNow.ng Redesign</p>
+        </div>
+        <div
+          className="project-list item5"
+          onClick={() => {
+            navigate(`/project/watertalk`);
+          }}
+        >
+          <p className="listName">WaterTalk</p>
         </div>
       </div>
     </>
