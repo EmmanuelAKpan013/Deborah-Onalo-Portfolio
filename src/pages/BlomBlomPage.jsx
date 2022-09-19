fd6710209689ea641d93d437f39c9d44abbfc800
EmmanuelAKpan013 <sundayemma013@gmail.com>
2022-09-19 10:44:13 +0100

    update

--- src/pages/BlomBlomPage.jsx
+++ src/pages/BlomBlomPage.jsx
@@ -20,15 +20,14 @@ function BlomBlomPage() {
         </div>
       </div>
 
-      {/*
-      <div className="roleContainer">
-        <div className="roleContent">
+      <div className="blom-roleContainer">
+        <div className="blom-roleContent">
           <h3>ROLE</h3>
           <br />
           <p>Research, UX Design</p>
           <p> UI Design.</p>
         </div>
-        <div className="roleContent">
+        <div className="blom-roleContent">
           <h3>TASK</h3>
           <br />
           <p>
@@ -36,12 +35,14 @@ function BlomBlomPage() {
             store and seamsless <br /> end to end appointment scheduling.
           </p>
         </div>
-        <div className="roleContent year">
+        <div className="blom-roleContent-year">
           <h3>YEAR</h3>
           <br />
           <p>2021 - Present</p>
         </div>
       </div>
+
+      {/*
       <div className="freeEyeTest">
         <img src={dashboardImage} alt="" />
       </div>
