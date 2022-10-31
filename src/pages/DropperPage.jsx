f1d377cd07bab2892d5459a26d8466f470b4f0c5
EmmanuelAKpan013 <sundayemma013@gmail.com>
2022-10-31 22:25:14 +0100

    Update

--- src/pages/DropperPage.jsx
+++ src/pages/DropperPage.jsx
@@ -1,7 +1,31 @@
-import React from "react";
+import "./DropperPage.css";
+import { motion } from "framer-motion";
+import Navbar from "../components/Navbar";
 
 function DropperPage() {
-  return <div>DropperPage</div>;
+  return (
+    <motion.div
+      initial={{ opacity: 0, transitionDuration: "2s" }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0, transitionDuration: "0.5s" }}
+    >
+      <Navbar />
+      <div className="dropper_nav">
+        <div className="dropper_navList">
+          <ul>
+            <li>Dropper</li>
+            <li>Services</li>
+            <li>Tracking</li>
+            <li>Location</li>
+          </ul>
+        </div>
+        <div className="dropper_navCreateAccount">
+          <p>Login</p>
+          <button>Create a free account</button>
+        </div>
+      </div>
+    </motion.div>
+  );
 }
 
 export default DropperPage;
